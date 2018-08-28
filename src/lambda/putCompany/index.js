e5983e3d0e3539e06deb4c8235e2417c782606d2
markus <marcholio09@gmail.com>
2018-08-28 22:00:34 +0300

    Package for lambdas

--- src/lambda/putCompany/index.js
+++ src/lambda/putCompany/index.js
@@ -1,5 +1,28 @@
+import AWS from 'aws-sdk';
+
 exports.handler = (event, context, callback) => {
   console.log(event);
+
+  AWS.config.update({ region: 'eu-central-1' });
+  const dbClient = new AWS.DynamoDB.DocumentClient();
+
+  const tableNamePrefix = 'VertaaTaksit_';
+
+  const params = {
+    Item: {
+      id: 'testcompany',
+      priceClasses: [{ persons: 3, price: 3.5 }, { persons: 4, price: 4 }],
+    },
+    TableName: `${tableNamePrefix}Companies`,
+  };
+
+  dbClient.put(params, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      console.log(data);
+    }
+  });
 
   var responseBody = {
     key3: 'value3',
