56d09b6435f9d3b15056b175eafd75e8d13f768d
markus <marcholio09@gmail.com>
2018-08-30 21:50:37 +0300

    Fixed deployment

--- deployLamdas.sh
+++ deployLamdas.sh
@@ -4,6 +4,9 @@ rm *.zip
 
 for f in *; do
     echo ${f}
-    zip ${f}.zip ${f}/*
+    cd ${f}
+    npm install
+    zip ${f}.zip *
     aws lambda update-function-code --function-name VertaaTaksit_${f} --zip-file fileb://${f}.zip --profile devuser
+    cd ..
 done
