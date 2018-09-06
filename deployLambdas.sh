397d9f7c7852cf69b38b9b92a11fe59632b81115
markus <marcholio09@gmail.com>
2018-09-06 22:26:10 +0300

    Get price lambda ready

--- deployLambdas.sh
+++ deployLambdas.sh
@@ -6,7 +6,7 @@ for f in *; do
     echo ${f}
     cd ${f}
     npm install
-    zip ${f}.zip *
+    zip -r ${f}.zip *
     aws lambda update-function-code --function-name VertaaTaksit_${f} --zip-file fileb://${f}.zip --profile devuser
     cd ..
 done
--- src/lambdas/getPrice/index.js
+++ src/lambdas/getPrice/index.js
@@ -1,13 +1,39 @@
 import GoogleMaps from '@google/maps';
+import AWS from 'aws-sdk';
+
+const tableNamePrefix = 'VertaaTaksit_';
+
+const getCompanies = () =>
+  new Promise((resolve, reject) => {
+    AWS.config.update({ region: 'eu-central-1' });
+    const dbClient = new AWS.DynamoDB.DocumentClient();
+    const params = {
+      TableName: `${tableNamePrefix}Companies`,
+    };
+    dbClient.scan(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        resolve(data.Items);
+      }
+    });
+  });
+
+const calculatePrice = (company, distance) => {
+  const startPrice = parseFloat(company.startPrice) || 0;
+  const pricePerKm = company.price || 0;
+  return startPrice + pricePerKm * distance;
+};
 
 exports.handler = (event, context, callback) => {
   const googleClient = GoogleMaps.createClient({
     key: 'AIzaSyAwhWuDLYC3VzSl9IZ7VpiZs0Vx__iky7c',
   });
-  if (!event.body) {
-    callback(null, { statusCode: 500, body: 'Missing request body' });
+  if (!event.queryStringParameters) {
+    callback(null, { statusCode: 500, body: 'Missing query string params' });
   } else {
-    const payload = JSON.parse(event.body);
+    const payload = event.queryStringParameters;
     if (!payload.from || !payload.to) {
       callback(null, { statusCode: 500, body: 'Missing from or to' });
     } else {
@@ -15,21 +41,50 @@ exports.handler = (event, context, callback) => {
         {
           origins: [payload.from],
           destinations: [payload.to],
-          units: 'metric',
         },
         (err, data) => {
           if (err) {
+            console.log(err);
             callback(null, {
               statusCode: 500,
               body: 'An exception occurred in Google Directions API',
             });
-            console.log(err);
           } else {
-            console.log(data);
+            const status = data.json.rows[0].elements[0].status;
+            if (status === 'NOT_FOUND') {
+              callback(null, { statusCode: 404, body: 'Not found' });
+            } else {
+              const distance =
+                data.json.rows[0].elements[0].distance.value / 1000;
+
+              getCompanies()
+                .then(companies => {
+                  callback(null, {
+                    statusCode: 200,
+                    headers: {
+                      'Access-Control-Allow-Origin': '*',
+                    },
+                    body: JSON.stringify({
+                      prices: companies
+                        .map(c => ({
+                          company: c.companyName,
+                          price: calculatePrice(c, distance),
+                        }))
+                        .sort((a, b) => a.price - b.price),
+                    }),
+                  });
+                })
+                .catch(err => {
+                  console.log(err);
+                  callback(null, {
+                    statusCode: 500,
+                    body: 'An exception occurred in Company DB',
+                  });
+                });
+            }
           }
         },
       );
-      callback(null, { statusCode: 200, body: 'OK' });
     }
   }
 };
