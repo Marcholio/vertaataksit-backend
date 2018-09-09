305b4ae991bcac0f39160e2fb8a8f3a8675cc9b1
markus <marcholio09@gmail.com>
2018-09-09 19:49:57 +0300

    Login

--- src/lambdas/login/index.js
+++ src/lambdas/login/index.js
@@ -7,42 +7,37 @@ exports.handler = (event, context, callback) => {
 
   const tableNamePrefix = 'VertaaTaksit_';
 
-  if (!event.body) {
-    callback(null, { statusCode: 500, body: 'Missing request body' });
+  if (!event.username || !event.password) {
+    callback(null, { statusCode: 500, body: 'Missing username or password' });
   } else {
-    const payload = JSON.parse(event.body);
-    if (!payload.username || !payload.password) {
-      callback(null, { statusCode: 500, body: 'Missing username or password' });
-    } else {
-      const params = {
-        Key: {
-          username: payload.username,
-        },
-        TableName: `${tableNamePrefix}Users`,
-      };
+    const params = {
+      Key: {
+        username: event.username,
+      },
+      TableName: `${tableNamePrefix}Users`,
+    };
 
-      dbClient.get(params, (err, data) => {
-        if (err) {
+    dbClient.get(params, (err, data) => {
+      if (err) {
+        callback(null, {
+          statusCode: 500,
+          body: 'An exception occurred',
+        });
+        console.log(err, err.stack);
+      } else {
+        const password = sha512(event.password);
+        if (password === data.Item.password) {
           callback(null, {
-            statusCode: 500,
-            body: 'An exception occurred',
+            statusCode: 200,
+            body: 'OK',
           });
-          console.log(err, err.stack);
         } else {
-          const password = sha512(payload.password);
-          if (password === data.Item.password) {
-            callback(null, {
-              statusCode: 200,
-              body: 'OK',
-            });
-          } else {
-            callback(null, {
-              statusCode: 403,
-              body: 'Forbidden',
-            });
-          }
+          callback(null, {
+            statusCode: 403,
+            body: 'Forbidden',
+          });
         }
-      });
-    }
+      }
+    });
   }
 };
